99679084ee63d434c15b33c79a2ee2e2dc97efe8
Daniel Halligan <dan.halligan@gmail.com>
2023-10-13 16:40:56 +0100

    Update solution

--- rosalind/bioinformatics_textbook_track/ba9r.py
+++ rosalind/bioinformatics_textbook_track/ba9r.py
@@ -1,6 +1,45 @@
 # Construct a Suffix Tree from a Suffix Array
 
-# from .ba9g import suffix_array
+
+class Node:
+    def __init__(self, parent=None, label=""):
+        self.parent = parent
+        self.label = label
+
+
+def desc(tree, node):
+    """
+    length of the concatenation of all path labels from the root to node
+    """
+    n = len(tree[node].label)
+    while tree[node].parent:
+        node = tree[node].parent
+        n += len(tree[node].label)
+    return n
+
+
+def ba9r(txt, sa, lcp):
+    tree = {-1: Node()}
+
+    for i in range(len(txt)):
+        v = i - 1
+        while tree[v].parent and desc(tree, v) > lcp[i]:
+            v = tree[v].parent
+        dv = desc(tree, v)
+
+        if lcp[i] == dv:
+            tree[i] = Node(v, txt[sa[i] + lcp[i] :])
+        else:
+            w = i - 1
+            while tree[w].parent and tree[w].parent != v:
+                w = tree[w].parent
+
+            tree[f"y{i}"] = Node(v, txt[sa[i - 1] + dv : sa[i - 1] + lcp[i]])
+            tree[w] = Node(f"y{i}", txt[sa[i - 1] + lcp[i] : sa[i - 1] + desc(tree, w)])
+            tree[i] = Node(f"y{i}", txt[sa[i] + lcp[i] :])
+
+    del tree[-1]
+    return tree
 
 
 def main(file):
@@ -8,38 +47,6 @@ def main(file):
     sa = [int(x) for x in sa.split(", ")]
     lcp = [int(x) for x in lcp.split(", ")]
 
-    # txt = "panamabananas$"
-    # sa = suffix_array(txt)
-    # print(sa)
-    # lcp = [0, 0, 1, 1, 3, 3, 1, 0, 0, 0, 2, 2, 0, 0]
-
-    edges = []
-    lcp.append(0)
-    levels = [0]
-    for i in range(len(txt)):
-        suffix = txt[sa[i] :]
-        before = lcp[i]
-        after = lcp[i + 1]
-        diff = after - before
-        if diff == 0:
-            edges.append(suffix[before:])
-        elif diff > 0:
-            edges.append(suffix[after:])
-            levels.append(after)
-        else:
-            edges.append(suffix[before:])
-            while True:
-                if levels[-2] == after:
-                    edges.append(suffix[after : levels[-1]])
-                    levels.pop()
-                    break
-                elif levels[-2] < after:
-                    edges.append(suffix[after : levels[-1]])
-                    levels.pop()
-                    levels.append(after)
-                    break
-                else:
-                    edges.append(suffix[levels[-2] : levels[-1]])
-                    levels.pop()
-                    continue
-    print(*sorted(edges), sep="\n")
+    tree = ba9r(txt, sa, lcp)
+    for key in tree.keys():
+        print(tree[key].label)
